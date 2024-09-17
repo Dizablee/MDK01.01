d32ac7b70e128d13796641ad57e0bcb1f2f01c85
Dizablee <mth78931@gmail.com>
2024-09-17 13:29:22 +0300

    Я решил задачу номер 1 Вариант 3

--- Program.cs
+++ Program.cs
@@ -10,8 +10,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Helllo!");   //Вывод текста на экран
-            Console.ReadKey();
+            List<int> numbers = new List<int> { -10, -5, -3, 0, 5, 7, 9, 10, 12, 15 }; //Список целых чисел
+            for (int i = numbers.Count - 1; i >= 0; i--)
+            {
+                if (numbers[i] >= -5 && numbers[i] < 10)
+                {
+                    numbers.RemoveAt(i);
+                }
+            }
+            Console.WriteLine("Список после удаления:");
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
