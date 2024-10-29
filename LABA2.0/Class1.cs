f138349b2d22ab858eb0771db74aa8d5f20d67c7
Dizablee <148859089+Dizablee@users.noreply.github.com>
2024-10-29 08:21:51 +0300

    Я сделал лабораторную работу №2.

---
+++ LABA2.0/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABA2._0
+{
+    
+        public enum Genre
+        {
+            RPG,
+            Action,
+            Strategy,
+            Horror,
+            Sport
+        }
+
+}
+
